e37cde439afc3d9d3a6614434782c4bec64df7ea
surpasses <hukevin2k03@gmail.com>
2025-12-28 02:05:26 +1300

    Fixed typo in app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from src.database import getStations
 from src.Railway import createNetwork
 
 app = Flask(__name__)
-CORS(app, origins=["https://train-project-six.vercel.app/", "http://localhost:5173"])
+CORS(app, origins=["https://train-project-six.vercel.app", "http://localhost:5173"])
 port = 5100
 
 # Initialize graph once at startup
